60cb1cd954dd081fc98b497e14364e8f2a0878f3
SREYNAJ KEB <101303611+Sreynaj@users.noreply.github.com>
2023-05-05 23:46:30 +0700

    progress

--- trydjango/playground/urls.py
+++ trydjango/playground/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello/', views.say_hello)
-]
+    path('hello', views.say_hello),
+    path('email/',views.email_gen)
+    ]
--- trydjango/playground/views.py
+++ trydjango/playground/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 # request --> response 
 def say_hello(request):
     return render(request,'hello.html')
+
+def email_gen(request):
+    #print("This is an email generator")
+    return render(request,"""<html><script>/Users/macuser/django/trydjango/playground/ChatGPT_Email.py;</script></html>""")
 
